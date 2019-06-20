2982a16c7385996f1df7efa4735006bc34e445c6
Maarten Esser <maartenjr.esser@gmail.com>
2019-06-20 16:27:09 +0200

    added more user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :memberships
   has_many :groups
+  # through: :memberships
+
+  has_many :invitations, class_name: "Invite", foreign_key: 'recipient_id'
+  has_many :sent_invites, class_name: "Invite", foreign_key: 'sender_id'
+
 end
