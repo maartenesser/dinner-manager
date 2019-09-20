e54fe4195ef0e6bec5a2b5c2ce8c886c142bddcf
Maarten Esser <maartenjr.esser@gmail.com>
2019-09-20 17:08:35 +0200

    Upgrade rails to 6.0.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,22 +1,9 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-
-  ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "dinnerchique@gmail.com",
-  :password             => "manager123456",
-  :authentication       => "plain"
-  # :enable_starttls_auto => true # I don't have this, but it should work anyway
-}
-
-
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -44,9 +31,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = true
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -60,7 +47,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
