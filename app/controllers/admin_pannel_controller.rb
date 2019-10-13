0e6af35737c5eb2586750ce77247c1ca7a06088a
Maarten Esser <maartenjr.esser@gmail.com>
2019-10-13 11:54:03 +0200

    finished the search query

--- app/controllers/admin_pannel_controller.rb
+++ app/controllers/admin_pannel_controller.rb
@@ -1,13 +1,27 @@
 class AdminPannelController < ApplicationController
   before_action :check_if_admin
+  # before_action :search_params, only: [:index]
 
   def index
-    @user = User.all
     # if just going to the main page the params are not there and it will give you an error
-    if params[:search][:query].present?
-      @user = User.all
+
+
+ # @cocktails = Cocktail.all
+    # @search = params["search"]
+    # if @search.present?
+    #   @name = @search["name"]
+    #   @cocktails = Cocktail.where(name: @name)
+    # end
+    @search = params[:search]
+    if @search.present?
+      @query = @search[:query]
+      if @query.empty?
+        @user = User.all
+      else
+        @user = User.where("last_name ILIKE ?", @query)
+      end
     else
-      @user = User.where("last_name LIKE ?", params[:search][:query])
+      @user = User.all
     end
   end
 
@@ -15,6 +29,10 @@ class AdminPannelController < ApplicationController
 
   def check_if_admin
     redirect_to root_path unless current_user.admin?
+  end
+
+  def search_params
+      params.require(:search).permit(:query)
   end
 
 end
